dbbb2901506ca15e5c87dfcf1fb3cc7f6085327e
jecko2 <89352917+jecko2@users.noreply.github.com>
2021-09-24 23:26:23 +0300

    Add files via upload

---
+++ MainHtmlFile/js/custom.js
@@ -0,0 +1,16 @@
+
+function showMenu (){
+    var element = document.getElementById("navigation-list");
+    element.classList.add("active")
+}
+
+
+
+function closeMenu (){
+    var element = document.getElementById("navigation-list");
+    element.classList.remove("active")
+}
+
+
+
+var book1 = document.getElementById
